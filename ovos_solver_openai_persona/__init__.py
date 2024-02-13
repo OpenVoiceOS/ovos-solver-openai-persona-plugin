cb8648a6059e01991cf9f55923e8a71a4a27b8de
JarbasAI <33701864+JarbasAl@users.noreply.github.com>
2024-02-13 17:29:20 +0000

    feat/streaming_solver (#4)
    implement streaming api

--- ovos_solver_openai_persona/__init__.py
+++ ovos_solver_openai_persona/__init__.py
@@ -38,8 +38,15 @@ class OpenAIPersonaSolver(OpenAIChatCompletionsSolver):
 
 
 if __name__ == "__main__":
-    bot = OpenAIPersonaSolver({"key": "sk-xxxx"})
-    print(bot.get_spoken_answer("describe quantum mechanics in simple terms"))
+    bot = OpenAIPersonaSolver({"key": "sk-xxxx", "api_url": "https://llama.smartgic.io/v1"})
+    for utt in bot.stream_utterances("describe quantum mechanics in simple terms"):
+        print(utt)
+        #  Quantum mechanics is a branch of physics that studies the behavior of atoms and particles at the smallest scales.
+        #  It describes how these particles interact with each other, move, and change energy levels.
+        #  Think of it like playing with toy building blocks that represent particles.
+        #  Instead of rigid structures, these particles can be in different energy levels or "states." Quantum mechanics helps scientists understand and predict these states, making it crucial for many fields like chemistry, materials science, and engineering.
+
+
     # Quantum mechanics is a branch of physics that deals with the behavior of particles on a very small scale, such as atoms and subatomic particles. It explores the idea that particles can exist in multiple states at once and that their behavior is not predictable in the traditional sense.
     print(bot.spoken_answer("Quem encontrou o caminho maritimo para o Brazil", {"lang": "pt-pt"}))
     # Explorador português Pedro Álvares Cabral é creditado com a descoberta do Brasil em 1500
--- ovos_solver_openai_persona/engines.py
+++ ovos_solver_openai_persona/engines.py
@@ -71,12 +71,11 @@ class OpenAIChatCompletionsSolver(QuestionSolver):
         self.memory = config.get("enable_memory", True)
         self.max_utts = config.get("memory_size", 15)
         self.qa_pairs = []  # tuple of q+a
-        self.current_q = None
-        self.current_a = None
         self.initial_prompt = config.get("initial_prompt", "You are a helpful assistant.")
 
     # OpenAI API integration
     def _do_api_request(self, messages):
+        s = requests.Session()
         headers = {
             "Content-Type": "application/json",
             "Authorization": "Bearer " + self.key
@@ -99,8 +98,45 @@ class OpenAIChatCompletionsSolver(QuestionSolver):
             # Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
             "stop": self.stop_token
         }
-        response = requests.post(self.api_url, headers=headers, data=json.dumps(payload)).json()
+        response = s.post(self.api_url, headers=headers, data=json.dumps(payload)).json()
         return response["choices"][0]["message"]["content"]
+
+    def _do_streaming_api_request(self, messages):
+
+        s = requests.Session()
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + self.key
+        }
+
+        # TODO - params from config
+        # https://platform.openai.com/docs/api-reference/completions/create
+        payload = {
+            "model": self.engine,
+            "messages": messages,
+            "max_tokens": 300,
+            "temperature": 1,
+            # between 0 and 2. Higher values like 0.8 will make the output more random, while lower values like 0.2 will make it more focused and deterministic.
+            "top_p": 1,
+            # nucleus sampling alternative to temperature, the model considers the results of the tokens with top_p probability mass. 0.1 means only tokens comprising top 10% probability mass are considered.
+            "n": 1,  # How many completions to generate for each prompt.
+            "frequency_penalty": 0,
+            # Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
+            "presence_penalty": 0,
+            # Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
+            "stop": self.stop_token,
+            "stream": True
+        }
+        for chunk in s.post(self.api_url, headers=headers,
+                            stream=True, data=json.dumps(payload)).iter_lines():
+            if chunk:
+                chunk = chunk.decode("utf-8")
+                chunk = json.loads(chunk.split("data: ", 1)[-1])
+                if chunk["choices"][0].get("finish_reason"):
+                    break
+                if "content" not in chunk["choices"][0]["delta"]:
+                    continue
+                yield chunk["choices"][0]["delta"]["content"]
 
     def get_chat_history(self, initial_prompt=None):
         qa = self.qa_pairs[-1 * self.max_utts:]
@@ -114,13 +150,21 @@ class OpenAIChatCompletionsSolver(QuestionSolver):
         return messages
 
     def get_prompt(self, utt, initial_prompt=None):
-        self.current_q = None
-        self.current_a = None
         messages = self.get_chat_history(initial_prompt)
         messages.append({"role": "user", "content": utt})
         return messages
 
     # officially exported Solver methods
+    def stream_utterances(self, query):
+        messages = self.get_prompt(query)
+        answer = ""
+        for chunk in self._do_streaming_api_request(messages):
+            answer += chunk
+            if any(chunk.endswith(p) for p in [".", "!", "?", "\n", ":"]):
+                if answer.strip():
+                    yield answer
+                answer = ""
+
     def get_spoken_answer(self, query, context=None):
         messages = self.get_prompt(query)
         response = self._do_api_request(messages)
