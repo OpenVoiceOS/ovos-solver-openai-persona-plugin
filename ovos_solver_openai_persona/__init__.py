aba8fd619086fc32ccf951675c48b194e198f339
miro <jarbasai@mailfence.com>
2025-01-31 03:55:23 +0000

    update parent class

--- ovos_solver_openai_persona/__init__.py
+++ ovos_solver_openai_persona/__init__.py
@@ -42,7 +42,7 @@ class OpenAIPersonaSolver(OpenAIChatCompletionsSolver):
         """
         messages = self.get_prompt(query, self.default_persona)
         response = self._do_api_request(messages)
-        answer = response.strip()
+        answer = response.strip() if response else ""
         if not answer or not answer.strip("?") or not answer.strip("_"):
             return None
         if self.memory:
@@ -65,8 +65,8 @@ LLAMA_DEMO = {
 
 if __name__ == "__main__":
     bot = OpenAIPersonaSolver({"key": "sk-xxxx",
-                               "api_url": "https://ollama.uoi.io/v1",
-                               "model": "hdnh2006/salamandra-7b-instruct"})
+                               "model": 'mixtral-8x7b',
+                               "api_url": "http://10.42.0.109:8401"})
     #for utt in bot.stream_utterances("describe quantum mechanics in simple terms"):
     #    print(utt)
         #  Quantum mechanics is a branch of physics that studies the behavior of atoms and particles at the smallest scales.
@@ -75,5 +75,5 @@ if __name__ == "__main__":
         #  Instead of rigid structures, these particles can be in different energy levels or "states." Quantum mechanics helps scientists understand and predict these states, making it crucial for many fields like chemistry, materials science, and engineering.
 
     # Quantum mechanics is a branch of physics that deals with the behavior of particles on a very small scale, such as atoms and subatomic particles. It explores the idea that particles can exist in multiple states at once and that their behavior is not predictable in the traditional sense.
-    print(bot.spoken_answer("Quem encontrou o caminho maritimo para o Brazil", lang="pt-pt"))
+    print(bot.spoken_answer("what is the definition of computer", lang="en-US"))
     # O português Pedro Álvares Cabral encontrou o caminho marítimo para o Brasil em 1500. Ele foi o responsável por descobrir o litoral brasileiro, embora Cristóvão Colombo tenha chegado à América do Sul em 1498, cinco anos antes. Cabral desembarcou na atual costa de Alagoas, no Nordeste do Brasil.
